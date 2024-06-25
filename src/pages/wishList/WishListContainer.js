7b57d97edf83447d4ed3467e9c43b36b78f0046a
maruanna <maruanna1994@gmail.com>
2024-06-25 15:49:44 +0900

    for pull

--- src/pages/wishList/WishListContainer.js
+++ src/pages/wishList/WishListContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import S from './style';
 import WishItemContents from './WishItemContents';
 import ResetHeader from '../layout/ResetHeader';
@@ -7,35 +7,74 @@ import ScrollEvent from '../layout/ScrollEvent';
 import NotWished from './NotWished';
 import Modal from './modal/Modal';
 import BasicButton from '../../components/button/BasicButton';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+
 
 const WishListContainer = () => {
     ResetHeader();
     ScrollEvent();
-    // RemoveHeader();
 
-
+    const [searchParams] = useSearchParams();
+    const [rooms, setRooms] = useState([]);
+    const [isWished, setIsWished] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(true); 
+    
     const navigate = useNavigate();
 
-    // 모달 상태 관리
-    const [isModalOpen, setIsModalOpen] = useState(true); 
+    // 로그인한 유저라고 가정
+    // const signInUser = {
+    //         id : '6672c609aa021dac908a48db',
+    //         userId : "hyeona023",
+    //         password : "1234",
+    //         name : "현아",
+    //         profileImg : "./images/pages/detail/user/1/thumb.jpg",
+    //         email : "hyeona@gmail.com",
+    //         address : {
+    //             city : "Kulas Light",
+    //             street : "Gwenborough",
+    //             suit : "Apt. 556",
+    //             zipcode : "92998-3874",
+    //         },
+    //         phone : "010-1234-1234",
+    //         // wishList 부분
+    //         wishList : [1,2,3]
+    //     };
+    
+    useEffect(()=> {
+        const getWishList = async () => {
+            try{
+                const userId = '6672c609aa021dac908a48db'; // 사용자 ID (임시로 하드코딩)
+                const response = await fetch(`http://localhost:8000/room/wishList?userId=${userId}&{searchparams.toString()}`);
+                const data = await response.json();
+                if(data && data.rooms) {
+                    setRooms(data.rooms);
+                    setIsWished(data.rooms.length > 0);
+                    console.log(data);
+                }
+            }catch(error){
+                console.error("Eroor fetching wishList:", error);
+            }
+
+        };
+        getWishList()
+
+    },[searchParams])
 
     // 모달창 닫는 함수
     const handleModalClose = () => {
         setIsModalOpen(false);
-        // WishItemContents를 보이게 함
-        setIsWished(true); 
+        
     };
-
-    // 유저가 찜한 숙소가 있는지 여부 상태
-    const [isWished, setIsWished] = useState(true);
 
 
     return (
         <S.WishListContainer>
             <S.PageTitle>위시리스트</S.PageTitle>
             {/* 찜한 숙소가 있다면 바로 위시리스트 페이지 보이게 하기 */}
-            {isWished ? <WishItemContents /> : (
+            {isWished ? (
+                <WishItemContents rooms={rooms} /> 
+            ): (
                     <div>
                         {/* 찜한 숙소가 없다면 모달창 먼저 띄우기. */}
                          {isModalOpen && (
